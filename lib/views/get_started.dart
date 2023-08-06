f910ab94c8eeb365c5860291f3dd6d76f4d4bf5d
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2023-08-06 21:36:14 +0100

    finished get started page

--- lib/views/get_started.dart
+++ lib/views/get_started.dart
@@ -1,5 +1,6 @@
 import 'package:bjayedu/routers/route_export.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 class GetStartedView extends StatelessWidget {
@@ -21,6 +22,30 @@ class GetStartedView extends StatelessWidget {
           ),
           Container(
             color: const Color(0xffE60CFF).withOpacity(.3),
+          ),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Welcome!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  'Start and learn\ntoday.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
           ),
           Positioned(
             bottom: 20,
